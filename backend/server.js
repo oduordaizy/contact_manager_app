edbaf0c1455d73d5b68351e140f3867672184918
achdaisy <oduordaizy@gmail.com>
2025-02-07 18:00:59 +0300

    Update: called the middleware for error handling

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 //creating the server
 const express = require("express")
+const { errorHandler } = require("./middleware/errorHandler")
 const dotenv = require("dotenv").config()
 const PORT = process.env.PORT || 5000
 
 //initializing the app
 const app = express()
 
+//Middleware that initializes a parser
+// The middleware will help parse data entered by the client in the body.
+app.use(express.json())
 app.use('/api/contacts', require("./routes/contactRoutes"))
+app.use(errorHandler)
 
 
 //listener
